65ad81176ec0cc03b3b386d8c384885aa039271e
Andreas K. Blindheim <andreas.kb@icloud.com>
2026-02-22 21:56:40 +0100

    Added very basic touch control using Button2 library. Holding the touch sensor now resets wifi credentials and restarts the esp. This function will be refactored.

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,35 @@
 #include <Arduino.h> // Core Arduino library
 #include <WiFi.h>
 #include <WiFiManager.h>
+#include "Button2.h"
 
 #define LED_PIN 13
 #define NUM_LEDS 16
+#define TPSENSOR1_PIN 14
+
+Button2 tpsensor1;
 
 #include <FastLED.h> // For LED control
 #include "lighthandler.h" // Custom LED animation functions
 
 // Forward declarations
 void WM_setup();
+void tpSensor1();
 
 void setup() {
 
   Serial.begin(115200);
+
+  tpsensor1.begin(TPSENSOR1_PIN, INPUT_PULLDOWN, false);
+  tpSensor1();
+
   WM_setup();
   ringsetup();
 
 }
 
 void loop() {
+  tpsensor1.loop();
   ringSpinner(CRGB::Green, 4, 1);
  
   FastLED.show(); 
@@ -36,6 +46,7 @@ void loop() {
 void WM_setup() {
   
   WiFiManager wm;
+  wm.setClass("invert");
   // wm.resetSettings();
 
   bool connected = wm.autoConnect("ExoPingAP", "password123"); //Future refactor: Make AP name based on chip ID.
@@ -47,4 +58,34 @@ void WM_setup() {
   }
 
   Serial.println("Connected to WiFi! IP Address: " + WiFi.localIP().toString());
+}
+
+
+
+
+void tpSensor1() {
+  //Handles tpsensor1!
+
+  //Handle button click event for tpsensor1
+  tpsensor1.setClickHandler([](Button2& b) {
+    
+    Serial.println("TPSensor1 clicked");  
+  });
+
+  //Handle button long click event for tpsensor1
+  tpsensor1.setLongClickHandler([](Button2& b) {
+    
+    Serial.println("Resetting WiFi settings...");
+    WiFiManager wm;
+    wm.resetSettings();
+    ESP.restart();
+  });
+
+  //Handle button double click event for tpsensor1
+  tpsensor1.setDoubleClickHandler([](Button2& b) {
+    
+    
+    Serial.println("TPSensor1 double clicked"); 
+  });
+
 }
